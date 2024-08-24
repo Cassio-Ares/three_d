c7f2eb9e609d5c70bc3e149286885230e19f2d5e
Cassio-Ares <cassiotadeu.ads@gmail.com>
2024-08-24 14:35:19 -0300

    ajuste de rodas e mock

--- src/components/mock/projectBD.js
+++ src/components/mock/projectBD.js
@@ -1,10 +1,11 @@
 import { TwoCubes } from "../view/componentsViews/twoCubes/TwoCubes";
 
 export const ComponentProjects = [
-    {
-        id: '1',
-        imgSrc: './site3D.gif',
-        nameProject: 'teste' ,
-        component: <TwoCubes/>
-    },
-]
+  {
+    id: "1",
+    imgSrc: "./site3D.gif",
+    nameProject: "teste",
+    component: <TwoCubes />,
+    text: "",
+  },
+];
--- src/components/view/showProject/ShowProject.js
+++ src/components/view/showProject/ShowProject.js
@@ -5,13 +5,15 @@ import { ComponentProjects } from "../../mock/projectBD.js";
 import { Btn_neon } from "@/components/view/componentsViews/btn_neon/Btn_neon.jsx";
 
 export const ShowProject = ({ id }) => {
-  const [project, setProject] = useState([]);
+  const [project, setProject] = useState(null);
+  const [text, setText] = useState(null)
 
   useEffect(() => {
     if (id) {
       const project = ComponentProjects.find((proj) => proj.id === id);
       if (project) {
         setProject(project.component);
+        setText(project.text)
       } else {
         console.error("Project not found for ID:", id);
       }
@@ -20,7 +22,10 @@ export const ShowProject = ({ id }) => {
 
   return (
     <>
-      {project}
+      {project ? project : <p>Loading...</p>}
+      <div className="contentText">
+       {text ? text : <p>Loading...</p>}
+      </div>
       <div className="contentBtn">
         <Btn_neon text_btn={"Retorne a galeria"} link={"/projects"} />
       </div>
